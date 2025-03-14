96b83556361d98979f055ae361463abbb44f99c1
William D. Jones <thor0505@comcast.net>
2025-03-14 01:27:22 -0400

    Refactor Boneless control program.

--- examples/icebreaker.py
+++ examples/icebreaker.py
@@ -144,7 +144,6 @@ def firmware(sample_rate):
               "load_adc",
               "wait_or_finish"
     ]
-
     ADC_TYPE = ["val", "idx_or_cmp", "idx_or_cmp", "none"]
 
     def jump_table(*args):
@@ -152,6 +151,7 @@ def firmware(sample_rate):
             return [resolver(arg) for arg in args]
         return relocate
 
+    RET_PTR = R0
     STAGE = R1
     DAC_VAL = R4
     ADC_VAL = R5
@@ -169,24 +169,24 @@ def firmware(sample_rate):
     jump_table(*STAGES),
 
     L("idle"),
-        LDXA(R2, UART_RX_RDY),
-        LDXA(R3, SAMPLE_RDY),
-        AND(R2, R2, R3),
-        ANDI(R2, R2, 1),
-        BZ1("idle"),
-        LDXA(XFER_TYPE, UART_RX_DATA),
+        JAL(RET_PTR, "sync_rx"),
+        MOV(XFER_TYPE, R3),
+    L("no_match"),
+        JAL(RET_PTR, "sync_sample"),
         LDXA(DAC_VAL, DAC_VAL_COPY),
-        # Keep xfer type around so we know whether to send one byte or two
-        # for each xfer.
-        LDX(ADC_VAL, XFER_TYPE, ADC_VAL_COPY),
-        MOVI(STAGE, STAGES.index("load_dac")),
-        J("loop"),
+        ANDI(R2, XFER_TYPE, 0b00011100),
+        BZ0("wait_match"),
+        J("save_adc"),
+    L("wait_match"),
+        SRLI(R2, R2, 2),
+        LDR(R3, R2, "sync_vals"),
+        CMP(R3, DAC_VAL),
+        BNE("no_match"),
+        J("save_adc"),
 
     L("load_dac"),
-        LDXA(R2, UART_TX_ACK),
-        ANDI(R2, R2, 1),
-        BZ1("load_dac"),
-        STXA(DAC_VAL, UART_TX_DATA),
+        MOV(R3, DAC_VAL),
+        JAL(RET_PTR, "wait_then_tx"),
         MOVI(STAGE, STAGES.index("load_adc")),
         J("loop"),
 
@@ -198,28 +198,19 @@ def firmware(sample_rate):
         jump_table(*ADC_TYPE),
 
     L("val"),
-        LDXA(R2, UART_TX_ACK),
-        ANDI(R2, R2, 1),
-        BZ1("val"),
-        STXA(ADC_VAL, UART_TX_DATA),
-        MOVI(STAGE, STAGES.index("wait_or_finish")),
-        J("loop"),
+        MOV(R3, ADC_VAL),
+        JAL(RET_PTR, "wait_then_tx"),
+        J("adc_epilog"),
 
     L("idx_or_cmp"),
-        LDXA(R2, UART_TX_ACK),
-        ANDI(R2, R2, 1),
-        BZ1("idx_or_cmp"),
-        STXA(ADC_VAL, UART_TX_DATA),
-    L("second_byte"),
-        LDXA(R2, UART_TX_ACK),
-        ANDI(R2, R2, 1),
-        BZ1("second_byte"),
-        SRLI(ADC_VAL, ADC_VAL, 8),
-        STXA(ADC_VAL, UART_TX_DATA),
-        MOVI(STAGE, STAGES.index("wait_or_finish")),
-        J("loop"),
+        MOV(R3, ADC_VAL),
+        JAL(RET_PTR, "wait_then_tx"),
+        SRLI(R3, R3, 8),
+        JAL(RET_PTR, "wait_then_tx"),
+        J("adc_epilog"),
 
     L("none"),
+    L("adc_epilog"),
         MOVI(STAGE, STAGES.index("wait_or_finish")),
         J("loop"),
 
@@ -229,22 +220,55 @@ def firmware(sample_rate):
         BZ1("finish"),
 
     L("wait"),
-        LDXA(R2, SAMPLE_RDY),
-        ANDI(R2, R2, 1),
-        BZ1("wait"),
-
-        LDXA(XFER_TYPE, UART_RX_DATA),
+        JAL(RET_PTR, "sync_sample"),
         LDXA(DAC_VAL, DAC_VAL_COPY),
-        # Keep xfer type around so we know whether to send one byte or two
-        # for each xfer.
-        LDX(ADC_VAL, XFER_TYPE, ADC_VAL_COPY),
-        MOVI(STAGE, STAGES.index("load_dac")),
-        J("loop"),
+        J("save_adc"),
 
     L("finish"),
         MOVI(CNT, 0),
         MOVI(STAGE, STAGES.index("idle")),
         J("loop"),
+
+    # Subroutines
+    # R0- Return, R3- Val to send, R2- Temp
+    L("wait_then_tx"),
+        LDXA(R2, UART_TX_ACK),
+        ANDI(R2, R2, 1),
+        BZ1("wait_then_tx"),
+        STXA(R3, UART_TX_DATA),
+        JR(R0, 0),
+
+    # R0- Return, R2- Temp
+    L("sync_sample"),
+        LDXA(R2, SAMPLE_RDY),
+        ANDI(R2, R2, 1),
+        BZ1("sync_sample"),
+        JR(R0, 0),
+
+    # R0- Return, R3- Received val, R2- Temp
+    L("sync_rx"),
+        LDXA(R2, UART_RX_RDY),
+        ANDI(R2, R2, 1),
+        BZ1("sync_rx"),
+        LDXA(R3, UART_RX_DATA),
+        JR(R0, 0),
+
+    # Tail call
+    L("save_adc"),
+        # Keep xfer type around so we know whether to send one byte or two
+        # for each xfer.
+        ANDI(R2, XFER_TYPE, 0b00000011),
+        LDX(ADC_VAL, R2, ADC_VAL_COPY),
+        MOVI(STAGE, STAGES.index("load_dac")),
+        J("loop"),
+
+    # Constant Data
+    L("sync_vals"),
+        0,  # None Val
+        0,
+        64,
+        127,
+        255
     ]
 
 
